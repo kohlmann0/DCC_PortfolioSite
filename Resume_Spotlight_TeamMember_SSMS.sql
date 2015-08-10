455c74c3eef4c5da5cb1670a7d270c7bd6f10124
unknown <mrunkle_he@dev12.bsecode.inc>
2015-08-10 14:28:33 -0500

    resume_spotlight_teamMember_SSMS

---
+++ Resume_Spotlight_TeamMember_SSMS.sql
@@ -0,0 +1,29 @@
+CREATE TABLE UserResume
+(
+	UserResumeID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+
+	ProfileID int FOREIGN KEY REFERENCES ContactProfile(ProfileID),
+	HtmlUpload varbinary(max) NOT NULL
+);
+
+CREATE TABLE ProjectSpotlight
+(
+	ProjectSpotlightID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+
+	ProfileID int FOREIGN KEY REFERENCES ContactProfile(ProfileID),
+	ProjectName varchar(255) NOT NULL,
+	Technologies varchar(255) NULL,
+	DevelopmentTime varchar(255) NULL,
+	ProjectDescription varchar(max) NULL,
+	RepoLink varchar(max) NULL,
+	Image_1 image NULL,
+	Image_2 image NULL
+);
+
+CREATE TABLE TeamMember
+(
+	TeamMemberID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+
+	ProfileID int FOREIGN KEY REFERENCES ContactProfile(ProfileID),
+	ProjectSpotlightID int FOREIGN KEY REFERENCES ProjectSpotlight(ProjectSpotlightID),
+);
