8fc1627bddf5a955cb3d2cf3386553f32b7512f3
unknown <rroethle_he@dev7.bsecode.inc>
2015-08-10 09:06:35 -0500

    Fixing Ryan's screw-up.

---
+++ DCC_PortfolioSite/DCC_PortfolioSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DCC_PortfolioSite.Startup))]
+namespace DCC_PortfolioSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
