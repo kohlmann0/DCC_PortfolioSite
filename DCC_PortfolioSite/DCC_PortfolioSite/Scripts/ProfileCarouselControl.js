8b0828979bbc88cae9e57c787d5b82808c2acaf6
unknown <mkohlmann_he@dev8.bsecode.inc>
2015-08-10 18:24:27 -0500

    Carousel working

---
+++ DCC_PortfolioSite/DCC_PortfolioSite/Scripts/ProfileCarouselControl.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function(){
+    console.log("TEST");
+    $('.ProfileCarousel').slick({
+        accessibility: true,
+        arrows: true,
+        dots: true,
+        infinite: true,
+        speed:300,
+        slidesToShow:1,
+        slidesToScroll:1
+ 
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+
+        });
+});
