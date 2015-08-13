8a312591f449e7e98798ac358e08f9c689b0799a
unknown <mkohlmann_he@dev8.bsecode.inc>
2015-08-13 10:10:19 -0500

    Formatted the About page better Formatted the Search Engine carousel better.

--- DCC_PortfolioSite/DCC_PortfolioSite/Scripts/SearchResultsCarouselControl.js
+++ DCC_PortfolioSite/DCC_PortfolioSite/Scripts/SearchResultsCarouselControl.js
@@ -1,18 +1,50 @@
 ﻿$(document).ready(function () {
     $('.SearchResultsCarousel').slick({
-        //accessibility: true,
-        //arrows: true,
+        accessibility: true,
+        arrows: true,
         centerMode: true,
         centerPadding: '5%',
         dots: true,
         infinite: true,
+        mobileFirst: true,
         speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1
-
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    accessibility: true,
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '5%',
+                    dots: true,
+                    infinite: true,
+                    mobileFirst: true,
+                    speed: 300,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    swipeToSlide: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    accessibility: true,
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '5%',
+                    dots: true,
+                    infinite: true,
+                    mobileFirst: true,
+                    speed: 300,
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    swipeToSlide: true,
+                }
+            }
+        ]
 
     });
 });
