bb5447660e463c8efeac7c4ccb9999e3e826173c
unknown <mkohlmann_he@dev8.bsecode.inc>
2015-08-13 15:15:19 -0500

    Project Format changes.

---
+++ DCC_PortfolioSite/DCC_PortfolioSite/Scripts/ProjectPreviewPartialControl.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $(document).on('input propertychange paste', function () {
+        $("#previewProjectName").html($("#ProjectName").val());
+        $("#previewTechnologies").html($("#Technologies").val());
+        $("#previewTeamMembers").html($("#TeamMembers").val());
+        $("#previewDevelopmentTime").html($("#DevelopmentTime").val());
+        $("#previewProjectDescription").html($("#ProjectDescription").val());
+        $("#previewRepoLink").html($("#RepoLink").val());
+
+    })
+});
