23f73a9cb97f60df50380f5964f936467f85bc8a
unknown <mkohlmann_he@dev8.bsecode.inc>
2015-08-14 11:11:43 -0500

    Authorization for Role Controller.

--- DCC_PortfolioSite/DCC_PortfolioSite/Controllers/RoleController.cs
+++ DCC_PortfolioSite/DCC_PortfolioSite/Controllers/RoleController.cs
@@ -21,12 +21,14 @@ namespace DCC_PortfolioSite.Controllers
         }
         
         // GET: Role
+        [Authorize(Roles = "Administrator")]
         public ActionResult Index()
         {
             var Roles = context.Roles.ToList();
             return View(Roles);
         }
 
+        [Authorize(Roles = "Administrator")]
         public ActionResult Create(string name)
         {
             var Role = new IdentityRole();
@@ -37,6 +39,7 @@ namespace DCC_PortfolioSite.Controllers
 
 
         [HttpPost]
+        [Authorize(Roles = "Administrator")]
         public ActionResult Create(IdentityRole Role)
         {
             context.Roles.Add(Role);
@@ -44,6 +47,7 @@ namespace DCC_PortfolioSite.Controllers
             return RedirectToAction("Index");
         }
 
+        [Authorize(Roles = "Administrator")]
         public ActionResult Delete(string name)
         {
             var Roles = context.Roles;
@@ -58,6 +62,7 @@ namespace DCC_PortfolioSite.Controllers
 
 
         [HttpPost]
+        [Authorize(Roles = "Administrator")]
         public ActionResult Delete(IdentityRole Role)
         {
             var Roles = context.Roles;
@@ -82,52 +87,47 @@ namespace DCC_PortfolioSite.Controllers
             return RedirectToAction("Index");
         }
 
- // POST: /Users/Delete/5
-[HttpPost, ActionName("DeleteUser")]
-[ValidateAntiForgeryToken]
-public async Task<ActionResult> DeleteConfirmed(string id)
-{
-  if (ModelState.IsValid)
-  {
-    if (id == null)
-    {
-      return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-    }
-    var _userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+         // POST: /Users/Delete/5
+        [HttpPost, ActionName("DeleteUser")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+          if (ModelState.IsValid)
+          {
+            if (id == null)
+            {
+              return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var _userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
     
-    var user = await _userManager.FindByIdAsync(id);
-    var logins = user.Logins;
+            var user = await _userManager.FindByIdAsync(id);
+            var logins = user.Logins;
 
-    foreach (var login in logins.ToList())
-    {
-      await _userManager.RemoveLoginAsync(login.UserId, new UserLoginInfo(login.LoginProvider, login.ProviderKey));
-    }
+            foreach (var login in logins.ToList())
+            {
+              await _userManager.RemoveLoginAsync(login.UserId, new UserLoginInfo(login.LoginProvider, login.ProviderKey));
+            }
 
-    var rolesForUser = await _userManager.GetRolesAsync(id);
+            var rolesForUser = await _userManager.GetRolesAsync(id);
 
-    if (rolesForUser.Count() > 0)
-    {
-      foreach (var item in rolesForUser.ToList())
-      {
-        // item should be the name of the role
-        var result = await _userManager.RemoveFromRoleAsync(user.Id, item);
-      }
-    }
+            if (rolesForUser.Count() > 0)
+            {
+              foreach (var item in rolesForUser.ToList())
+              {
+                // item should be the name of the role
+                var result = await _userManager.RemoveFromRoleAsync(user.Id, item);
+              }
+            }
 
-    await _userManager.DeleteAsync(user);
+            await _userManager.DeleteAsync(user);
 
-    return RedirectToAction("Index");
-  }
-  else
-  {
-    return View();
-  }
-}
-
-
-
-
-
-
+            return RedirectToAction("Index");
+          }
+          else
+          {
+            return View();
+          }
+        }
     }
 }
