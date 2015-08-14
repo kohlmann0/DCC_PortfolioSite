78a1f8a243bf636c26916a5082ab3fd5bcc1614f
unknown <mkohlmann_he@dev8.bsecode.inc>
2015-08-14 17:48:24 -0500

    Updated Admin Controller

--- DCC_PortfolioSite/DCC_PortfolioSite/Controllers/AdminController.cs
+++ DCC_PortfolioSite/DCC_PortfolioSite/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
@@ -22,43 +23,16 @@ namespace DCC_PortfolioSite.Controllers
         [Authorize]
         public ActionResult SaveProfile(AdminViewModel model)
         {
-            string connectionStringDB = "Server=tcp:wx9a1lruht.database.windows.net,1433;Database=DCCPortfolioSite_db;User ID=devcodecamp;Password=heliumdev1!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30";
-            using (SqlConnection connection = new SqlConnection(connectionStringDB))
+            if (ModelState.IsValid)
             {
-                connection.Open();
-
-                SqlCommand cmd = new SqlCommand("UPDATE ContactProfile SET FirstName = @fName, LastName = @lName, BirthDate = @bDate, PrimaryPhone = @pPhone, AlternatePhone = @aPhone, StreetAddress = @sAddress, City = @city, USAState = @usaState, Country = @country, PostalCode = @pCode, About = @about, Photo = @photo, LinkedIn = @lIn, GitHub = @gHub, PersonalWebsite = @pWebSite, AvailableForWork = @aForWork, HiredBy = @hBy, CurrentWork = @cWork WHERE PrimaryEmail = @fNameUser");
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = connection;
-
-                cmd.Parameters.AddWithValue("@fName", (String.IsNullOrEmpty(model.ContactProfile.FirstName)) ? " " : model.ContactProfile.FirstName) ;
-                cmd.Parameters.AddWithValue("@lName", (String.IsNullOrEmpty(model.ContactProfile.LastName)) ? " " : model.ContactProfile.LastName);
-                cmd.Parameters.AddWithValue("@bDate", (DateTime.Equals(model.ContactProfile.BirthDate, null)) ? Convert.ToDateTime("01/01/1900") : model.ContactProfile.BirthDate);
-                cmd.Parameters.AddWithValue("@pPhone", (String.IsNullOrEmpty(model.ContactProfile.PrimaryPhone)) ? " " : model.ContactProfile.PrimaryPhone);
-                cmd.Parameters.AddWithValue("@aPhone", (String.IsNullOrEmpty(model.ContactProfile.AlternatePhone)) ? " " : model.ContactProfile.AlternatePhone);
-                cmd.Parameters.AddWithValue("@pEmail", (String.IsNullOrEmpty(model.ContactProfile.PrimaryEmail)) ? " " : model.ContactProfile.PrimaryEmail);
-                cmd.Parameters.AddWithValue("@aEmail", (String.IsNullOrEmpty(model.ContactProfile.AlternateEmail)) ? " " : model.ContactProfile.AlternateEmail);
-                cmd.Parameters.AddWithValue("@sAddress", (String.IsNullOrEmpty(model.ContactProfile.StreetAddress)) ? " " : model.ContactProfile.StreetAddress);
-                cmd.Parameters.AddWithValue("@city", (String.IsNullOrEmpty(model.ContactProfile.City)) ? " " : model.ContactProfile.City);
-                cmd.Parameters.AddWithValue("@usaState", (String.IsNullOrEmpty(model.ContactProfile.USAState)) ? " " : model.ContactProfile.USAState);
-                cmd.Parameters.AddWithValue("@country", (String.IsNullOrEmpty(model.ContactProfile.Country)) ? " " : model.ContactProfile.Country);
-                cmd.Parameters.AddWithValue("@pCode", (int.Equals(model.ContactProfile.PostalCode, null)) ? 0 : model.ContactProfile.PostalCode);
-                cmd.Parameters.AddWithValue("@about", (String.IsNullOrEmpty(model.ContactProfile.About)) ? " " : model.ContactProfile.About);
-                cmd.Parameters.AddWithValue("@photo", (String.IsNullOrEmpty(model.ContactProfile.Photo)) ? " " : model.ContactProfile.Photo);
-                cmd.Parameters.AddWithValue("@lIn", (String.IsNullOrEmpty(model.ContactProfile.LinkedIn)) ? " " : model.ContactProfile.LinkedIn);
-                cmd.Parameters.AddWithValue("@gHub", (String.IsNullOrEmpty(model.ContactProfile.GitHub)) ? " " : model.ContactProfile.GitHub);
-                cmd.Parameters.AddWithValue("@pWebsite", (String.IsNullOrEmpty(model.ContactProfile.PersonalWebsite)) ? " " : model.ContactProfile.PersonalWebsite);
-                cmd.Parameters.AddWithValue("@aForWork", (bool.Equals(model.ContactProfile.AvailableForWork, null)) ? true : model.ContactProfile.AvailableForWork);
-                cmd.Parameters.AddWithValue("@hBy", (String.IsNullOrEmpty(model.ContactProfile.HiredBy)) ? " " : model.ContactProfile.HiredBy);
-                cmd.Parameters.AddWithValue("@cWork", (String.IsNullOrEmpty(model.ContactProfile.CurrentWork)) ? " " : model.ContactProfile.CurrentWork);
-                
-                cmd.Parameters.AddWithValue("@fNameUser", User.Identity.Name);
-
-                cmd.ExecuteNonQuery();
-
+                db.Entry(model.ContactProfile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Admin");
             }
-
-            return RedirectToAction("Index", "Admin");
+            else
+            {
+                return View(model);
+            }
         }
 
 
@@ -67,30 +41,16 @@ namespace DCC_PortfolioSite.Controllers
         [Authorize]
         public ActionResult SaveProject(AdminViewModel model)
         {
-            string connectionStringDB = "Server=tcp:wx9a1lruht.database.windows.net,1433;Database=DCCPortfolioSite_db;User ID=devcodecamp;Password=heliumdev1!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30";
-            using (SqlConnection connection = new SqlConnection(connectionStringDB))
+            if (ModelState.IsValid)
             {
-                connection.Open();
-
-                SqlCommand cmd = new SqlCommand("UPDATE ContactProfile SET ProjectName = @pName, Technologies = @tech, DevelopmentTime = @dTime, ProjectDescription = @pDescrip, RepoLink = @rLink, Image_1 = @i1, Image_2 = @i2 WHERE PrimaryEmail = @fNameUser");
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = connection;
-
-                cmd.Parameters.AddWithValue("@pName", (String.IsNullOrEmpty(model.ProjectSpotlight.ProjectName)) ? " " : model.ProjectSpotlight.ProjectName);
-                cmd.Parameters.AddWithValue("@tech", (String.IsNullOrEmpty(model.ProjectSpotlight.Technologies)) ? " " : model.ProjectSpotlight.Technologies);
-                cmd.Parameters.AddWithValue("@dTime", (String.IsNullOrEmpty(model.ProjectSpotlight.DevelopmentTime)) ? " " : model.ProjectSpotlight.DevelopmentTime);
-                cmd.Parameters.AddWithValue("@pDescrip", (String.IsNullOrEmpty(model.ProjectSpotlight.ProjectDescription)) ? " " : model.ProjectSpotlight.ProjectDescription);
-                cmd.Parameters.AddWithValue("@rLink", (String.IsNullOrEmpty(model.ProjectSpotlight.RepoLink)) ? " " : model.ProjectSpotlight.RepoLink);
-                cmd.Parameters.AddWithValue("@i1", (String.IsNullOrEmpty(model.ProjectSpotlight.Image_1)) ? " " : model.ProjectSpotlight.Image_1);
-                cmd.Parameters.AddWithValue("@i2", (String.IsNullOrEmpty(model.ProjectSpotlight.Image_2)) ? " " : model.ProjectSpotlight.Image_2);
-
-                cmd.Parameters.AddWithValue("@fNameUser", User.Identity.Name);
-
-                cmd.ExecuteNonQuery();
-
+                db.Entry(model.ProjectSpotlight).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Admin");
             }
-
-            return RedirectToAction("Index", "Admin");
+            else
+            {
+                return View(model);
+            }
         }
 
         // GET: Admin
@@ -98,10 +58,8 @@ namespace DCC_PortfolioSite.Controllers
         public ActionResult Index()
         {
             AdminViewModel adminViewModel = new AdminViewModel();
+            string UserID = User.Identity.Name;            
 
-            string UserID = User.Identity.Name;
-
-            
             adminViewModel.ContactProfile = db.ContactProfiles.FirstOrDefault(p => p.PrimaryEmail == UserID);
             adminViewModel.UserResume = db.UserResumes.FirstOrDefault(p => p.ContactProfile.PrimaryEmail == UserID);
             adminViewModel.ProjectSpotlight = db.ProjectSpotlights.FirstOrDefault(p => p.ContactProfile.PrimaryEmail == UserID);
@@ -110,11 +68,6 @@ namespace DCC_PortfolioSite.Controllers
                            select p).ToList();
             adminViewModel.ProjectSpotlightList = projects;
             
-            //adminViewModel.UserProfile = db.UserProfiles.FirstOrDefault(p => p.UserID == 2);
-            //adminViewModel.ContactProfile = db.ContactProfiles.FirstOrDefault(p => p.ProfileId == 2);
-            //adminViewModel.UserResume = db.UserResumes.FirstOrDefault(p => p.UserResumeID == 2);
-            //adminViewModel.ProjectSpotlight = db.ProjectSpotlights.FirstOrDefault(p => p.ProjectSpotlightID == 3);
-
             return View(adminViewModel);
         }
 
